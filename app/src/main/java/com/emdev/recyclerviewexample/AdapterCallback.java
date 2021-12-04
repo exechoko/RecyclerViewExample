73da6077f9570b812e7b08914e4cb0851567c94a
Exequiel <49988126+exechoko@users.noreply.github.com>
2021-12-04 07:54:58 -0300

    Update AdapterCallback.java

--- app/src/main/java/com/emdev/recyclerviewexample/AdapterCallback.java
+++ app/src/main/java/com/emdev/recyclerviewexample/AdapterCallback.java
@@ -2,9 +2,6 @@ package com.emdev.recyclerviewexample;
 
 import com.emdev.recyclerviewexample.model.Persona;
 
-/**
- * @author  Eduardo Medina
- */
 public interface AdapterCallback {
 
     void showDialog(Persona item);
